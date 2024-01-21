bb2a971cefcdfe0335049de08f19ae4be7979197
saxomaniac <Daniel.mc21@yahoo.com>
2024-01-21 10:24:10 -0500

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,22 @@
+/**
+* This Program is deisgned to calculate the 9th term in the fibonacci sequence and print the results.
+    */
 public class Fibonacci {
+    /**
+    *   The main method for our Fibonacci Program
+    * @Param  args ( not relevant )
+    */ 
     public static void main(String[] args) {
         int n = 10; 
         int result = fibonacci(n);
         System.out.println("The " + n + "th Fibonacci term is: " + result);
     }
 
+    /**
+    * Math equation for our fibonacci calculation
+    * @param variable to select which fibonacci term will be found
+    * @return nth term in the Fibonacci sequence
+    */
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
